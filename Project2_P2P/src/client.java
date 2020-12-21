6fe11e23eb3611419b59966285d7e85f99b7e85a
LOPEDOTE Victor <viclope@live.fr>
2020-12-21 11:14:59 +0100

    Commit of the client.java and sever.java files (UDP)

---
+++ Project2_P2P/src/client.java
@@ -0,0 +1,55 @@
+// client.java
+
+import java.net.*;
+import java.io.*;
+
+public class client
+{
+   public static void main(String [] args) throws IOException
+   {
+//      String serverName = args[0];
+      InetAddress serverName = InetAddress.getByName("localhost"); 			
+      int port = 4444;
+
+      //console stream
+      BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+      
+      try
+      {
+	 //client datagram socket and packet 
+	 DatagramSocket client=new DatagramSocket();
+	 DatagramPacket packet = null;
+
+	 //input and output streams
+	 byte[] buffer=new byte[256];
+	 String outMessage="";
+	 String inMessage="";
+
+//	 while(!inMessage.equals("exit")){
+	 //entering the message to send
+         System.out.println("Please type message to send : ");
+	 outMessage = stdIn.readLine();
+
+	 //sending message
+	 buffer = outMessage.getBytes();
+	 packet=new DatagramPacket(buffer, buffer.length, serverName, port);
+	 client.send(packet);
+
+	 //receiving message
+	 packet = new DatagramPacket(buffer, buffer.length);
+	 client.receive(packet);
+
+	 inMessage = new String(packet.getData(), 0, packet.getLength());
+         System.out.println("[Received message] -> "+inMessage);
+//	 }
+
+	 //closing socket
+         client.close();
+	 System.out.println("Socket closed");
+
+      }catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
